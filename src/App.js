08307b185a3ce087cc11706ca16b998323ee508e
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-01-06 14:46:35 +0200

    Movie

--- src/App.js
+++ src/App.js
@@ -129,18 +129,24 @@ function MovieList() {
   return (
     <ul className="list">
       {movies?.map((movie) => (
-        <li key={movie.imdbID}>
-          <img src={movie.Poster} alt={`${movie.Title} poster`} />
-          <h3>{movie.Title}</h3>
-          <div>
-            <p>
-              <span>🗓</span>
-              <span>{movie.Year}</span>
-            </p>
-          </div>
-        </li>
+        <Movie movie={movie}key={movie.imdbID} />
       ))}
     </ul>
+  );
+}
+
+function Movie({movie}) {
+  return (
+    <li >
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
   );
 }
 
