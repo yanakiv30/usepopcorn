37fe2442739996c1793e77c3495cae64c5fbe20e
yanakiv30 <yanakiv30@gmail.com>
2024-01-09 15:16:57 +0200

    Revert "Movie"
    This reverts commit 08307b185a3ce087cc11706ca16b998323ee508e.

--- src/App.js
+++ src/App.js
@@ -130,24 +130,18 @@ function MovieList({movies}) {
   return (
     <ul className="list">
       {movies?.map((movie) => (
-        <Movie movie={movie}key={movie.imdbID} />
+        <li key={movie.imdbID}>
+          <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          <h3>{movie.Title}</h3>
+          <div>
+            <p>
+              <span>🗓</span>
+              <span>{movie.Year}</span>
+            </p>
+          </div>
+        </li>
       ))}
     </ul>
-  );
-}
-
-function Movie({movie}) {
-  return (
-    <li >
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
-      <div>
-        <p>
-          <span>🗓</span>
-          <span>{movie.Year}</span>
-        </p>
-      </div>
-    </li>
   );
 }
 
