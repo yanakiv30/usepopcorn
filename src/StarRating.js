000aca7316c5fbb258f9558489d73ec7ab9b01f8
yanakiv30 <yanakiv30@gmail.com>
2024-01-12 18:31:04 +0200

    PropTypes

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -8,6 +9,16 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number, 
+  defaultRating: PropTypes.number,
+  size: PropTypes.number,
+  color: PropTypes.string,
+  className: PropTypes.string,
+  messages: PropTypes.array,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
@@ -25,7 +36,7 @@ export default function StarRating({
 function handleRating(rating) {
   
     setRating(rating+1);
-    onSetRating(rating+1);
+    onSetRating ? onSetRating(rating+1) :  console.log("onset not");
   
  
 }
