12e3a8737ed09aa3ad22eee532193821237f498e
yanakiv30 <yanakiv30@gmail.com>
2024-01-14 22:51:11 +0200

    useEffect

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,4 +1,4 @@
-import { useEffect,  useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,18 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "69fcf81a"
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-
-useEffect(function() {
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-  .then((res) => res.json())
-  .then((data) => setMovies(data.Search));
-},[]);
-
- 
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -75,9 +66,9 @@ useEffect(function() {
           <MovieList movies={movies} />
         </Box>
 
-        <Box>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
+        <Box>          
+            <WatchedSummary watched={watched} />
+            <WatchedMoviesList watched={watched} />          
         </Box>
 
       </Main>
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './StarRating';
+import './index.css';
+import App from './App';
+// import StarRating from './StarRating';
 
 
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  )
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   )
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <App />
+    {/* <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
     <StarRating
       size={24}
       color='red'
@@ -28,6 +28,6 @@ root.render(
       defaultRating={3}
       
     />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
